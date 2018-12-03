2930046f753ff88c1b6d660131240060c36bb923
Seth Berrier <berriers@uwstout.edu>
2018-12-03 14:20:48 -0600

    Updating to include uglify

--- yarnall.sh
+++ yarnall.sh
@@ -3,7 +3,6 @@ for file in */ ; do
   if [[ -d "${file}/play" && ! -L "$file" ]]; then
     cd "${file}/play"
     rm package-lock.json
-    npm update uglifyjs-webpack-plugin
     yarn
     npm run sfx
     npm run deploy
