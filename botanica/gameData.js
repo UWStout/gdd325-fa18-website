92b0bc55b5e95e3059abf8f856982c4c7b5d5682
Seth Berrier <berriers@uwstout.edu>
2018-12-04 12:38:03 -0600

    Update to BOTANICA gameData

--- botanica/gameData.js
+++ botanica/gameData.js
@@ -9,19 +9,24 @@ var gameData = {
   playDestination: 'play/index.html',
 
   /** Please do not edit things ABOVE this line **/
-  byline: 'A pithy and short statement to catch the players attention!',
+  byline: 'escape the greenhouse and make it back to the great outdoors',
 
   description: `
-  description goes here.
+  You are a cricket named Curtis, trapped in a greenhouse. You must navigate
+  your way through the environment while avoiding all the other types of bugs
+  in your path. You are trying to escape the greenhouse and make it back to
+  the great outdoors.
   `,
 
   creators: [
-    { name: 'Creator name', contrib: 'Title and/or What I did for the game' },
-    { name: 'Creator name', contrib: 'Title and/or What I did for the game' }
+    { name: 'Joey Burss', contrib: 'Programmer' },
+    { name: 'Kenley Smith', contrib: 'Programmer, Sheriff' },
+    { name: 'Cailey Kocian', contrib: 'Programmer' },
+    { name: 'Trisha Kriegel', contrib: 'Artist' }
   ],
 
   otherContributors: [
-    { name: 'Outside Contributor name', contrib: 'Title and/or What they did for the game' }
+    // { name: 'Outside Contributor name', contrib: 'Title and/or What they did for the game' }
   ],
 
   screenshots: [
